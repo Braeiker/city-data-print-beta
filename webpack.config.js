00aec2ad5192d3ef8db22d068454a3435e2f51dc
Braeiker <samueletantillo@gmail.com>
2023-08-08 11:21:21 +0200

    fix 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin=require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
-  entry: './src/JS/script.js',
+  entry: {
+    index: path.resolve(__dirname, "./src/JS/script.js")
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'script.bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./index.html")
+    })
+  ]
 };
 
